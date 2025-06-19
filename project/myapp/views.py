256ca549647127c2d5a57bc2d15c350a26504a3c
brucebett <bett6448@gmail.com>
2025-06-19 21:51:22 +0300

    8thur commit

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
 def contact(request):
     return render(request, 'contact.html')
+
+def academics(request):
+    return render(request, 'academics.html')
+
+def alumni(request):
+    return render(request, 'alumni.html')
+
+def admision(request):
+    return render(request, 'admision.html')
+
+def event(request):
+    return render(request, 'event.html')
