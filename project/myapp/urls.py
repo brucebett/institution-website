964d72c124fa1c4bd8e7b5748f5caf765a5d3a36
brucebett <bett6448@gmail.com>
2025-06-19 21:56:30 +0300

    9thur commit

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('contact/', views.contact, name='contact')
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('admision/', views.admision, name='admision'),
+    path('academics/', views.academics, name='academics'),
+    path('alumni/', views.alumni, name='alumni'),
 ]
